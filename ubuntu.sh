09d0849ba660a770c294468d75a1f3bc05328bf3
Quyenhhh <106009036+Quyenhhh@users.noreply.github.com>
2026-04-19 13:51:55 +0700

    Delete ubuntu.sh

--- ubuntu.sh
+++
@@ -1,2 +0,0 @@
-apt update && apt install sudo nano wget git adduser -y && wget https://archive.org/download/ubuntumodontermux/sudoers && rm /etc/sudoers && mv sudoers /etc/sudoers && adduser ubuntu && wget https://archive.org/download/ubuntumodontermux/ubuntumod.sh && mv ubuntumod.sh /home/ubuntu/ubuntumod.sh && su ubuntu
-
